d6735a480a6fc54b779531fd3ce2e1228de1a639
namsujin <a25062401@gmail.com>
2026-04-22 11:25:12 +0900

    feat: token 인증 에러처리

--- src/main/java/org/accompany/backend/global/code/ErrorCode.java
+++ src/main/java/org/accompany/backend/global/code/ErrorCode.java
@@ -16,8 +16,13 @@ public enum ErrorCode {
 	INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_005", "서버 오류"),
 
 	// 사용자
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "사용자를 찾을 수 없습니다.")
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "사용자를 찾을 수 없습니다."),
 
+	// 인증
+	REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH_001", "리프레시 토큰이 없습니다."),
+	INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 리프레시 토큰입니다."),
+	STORED_REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH_003", "저장된 리프레시 토큰이 없습니다."),
+	EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_004", "만료된 리프레시 토큰입니다.")
 	;
 	private final HttpStatus status;
 	private final String code;
