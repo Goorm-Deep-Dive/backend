b9a1e7c3300bc0799275a35e91e4ca78a735854e
EUNSEONG LEE <chloe@EUNSEONGs-MacBook-Pro.local>
2026-04-28 01:18:22 +0900

    fix/getCategoryProcedures수정 #40

--- src/main/java/org/accompany/backend/domain/checklist/service/ChecklistService.java
+++ src/main/java/org/accompany/backend/domain/checklist/service/ChecklistService.java
@@ -11,6 +11,6 @@ public interface ChecklistService {
 	ChecklistCategoryProcedureRes getCategoryProcedures(Long categoryId, Long userId);
 	ChecklistProcedureDetailRes getProcedureDetail(Long procedureId, Long userId);
 	ChecklistOverallProgressRes getOverallProgress(Long userId);
-	void modifyProcedureCheck(Long checklistId, Long userId, boolean isChecked);
+	void modifyProcedureCheck(Long userProcedureChecklistId, Long userId, boolean isChecked);
 	void modifyDocumentCheck(Long procedureDocumentId, Long userId, boolean isChecked);
 }
